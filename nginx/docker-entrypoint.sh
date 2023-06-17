6f36e561de18ed1f88da339a429e78a7059fcb27
geor128 <geor128@gmail.com>
2023-06-17 15:11:00 -0300

    atualização para ordenar levantamento dos container2

---
+++ nginx/docker-entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+# Inicie o Nginx em segundo plano
+nginx -g "daemon off;"
---
+++ nodejs/docker-entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Comandos e instruções antes de iniciar o aplicativo Node.js
+
+# Iniciar o aplicativo Node.js
+node index.js
