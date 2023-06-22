465938d7bc0f1c00372c8609de20ab29330ade18
geor128 <geor128@gmail.com>
2023-06-22 17:15:14 -0300

    refatorando

--- nginx/docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-# Inicie o Nginx em segundo plano
-nginx -g "daemon off;"
--- nodejs/docker-entrypoint.sh
+++ nodejs/docker-entrypoint.sh
@@ -2,5 +2,8 @@
 
 # Comandos e instruções antes de iniciar o aplicativo Node.js
 
+# npm install sendo executado dentro do container
+npm install
+
 # Iniciar o aplicativo Node.js
 node index.js
